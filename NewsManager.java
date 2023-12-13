d8eeec0ced76822b614b351c455915693c856169
YerasulGOD20 <sacterasyl@gmail.com>
2023-12-14 01:20:05 +0600

    Update NewsManager.java

--- NewsManager.java
+++ NewsManager.java
@@ -1,76 +1,41 @@
 package proj;
 
-
-/**
-* @generated
-*/
 public class NewsManager extends Manager {
-    
-    
-    /**
-    * @generated
-    */
     private News news;
-    
-    
 
-    //                          Operations                                  
-    
-    /**
-    * @generated
-    */
-    public void addNewsArticle() {
-        //TODO
-        return null;
+    public NewsManager(News news) {
+        this.news = news;
     }
+
     
-    /**
-    * @generated
-    */
+    private News news;
+
+    public void addNewsArticle(LocalDate postDate, String title, String content, User user) {
+        this.news = new News(postDate, title, content, user);
+    }
     public void viewNewsArticle() {
-        //TODO
-        return null;
+        System.out.println(news.toString());
     }
-    
-    /**
-    * @generated
-    */
+
     public void makePostInsta() {
-        //TODO
+
         return null;
     }
-    
-    /**
-    * @generated
-    */
     public void makeStoriesInsta() {
-        //TODO
         return null;
     }
-    
-    /**
-    * @generated
-    */
     public void makePostTelega() {
-        //TODO
         return null;
     }
-    
-    /**
-    * @generated
-    */
-    public void changeNewsArticle() {
-        //TODO
-        return null;
+    public void changeNewsArticle(String title, String content) {
+        this.news.setTitle(title);
+        this.news.setContent(content);
     }
-    
-    /**
-    * @generated
-    */
     public boolean deleteNews() {
-        //TODO
-        return false;
+        this.news = null;
+        return true;
     }
+    
     
     
 }
