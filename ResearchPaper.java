7dd7215ba736874214462d35e09c79bbf9bff5a7
YerasulGOD20 <sacterasyl@gmail.com>
2023-12-14 01:19:24 +0600

    Update ResearchPaper.java

--- ResearchPaper.java
+++ ResearchPaper.java
@@ -1,52 +1,17 @@
 package proj;
-
-
-/**
-* @generated
-*/
 public class ResearchPaper {
-    
-    /**
-    * @generated
-    */
     private String tittle;
-    
-    /**
-    * @generated
-    */
     private String content;
-    
-    
-    /**
-    * @generated
-    */
     private ResearchProject researchProject;
-    
-    
-    /**
-    * @generated
-    */
     public String getTittle() {
         return this.tittle;
     }
-    
-    /**
-    * @generated
-    */
     public String setTittle(String tittle) {
         this.tittle = tittle;
     }
-    
-    /**
-    * @generated
-    */
     public String getContent() {
         return this.content;
     }
-    
-    /**
-    * @generated
-    */
     public String setContent(String content) {
         this.content = content;
     }
