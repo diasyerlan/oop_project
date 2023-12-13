141d671cd66485c0acf70dd0963edbfb9b8c5996
YerasulGOD20 <sacterasyl@gmail.com>
2023-12-14 01:21:12 +0600

    Update ResearcherDecorator.java

--- ResearcherDecorator.java
+++ ResearcherDecorator.java
@@ -1,47 +1,17 @@
 package proj;
-
-
-/**
-* @generated
-*/
 public class ResearcherDecorator implements Researcher {
     
-    /**
-    * @generated
-    */
     private CanBeResearcher researcher;
-    
-    /**
-    * @generated
-    */
     private CanBeResearcher researcher;
-    
-    
-    
-    /**
-    * @generated
-    */
     public CanBeResearcher getResearcher() {
         return this.researcher;
     }
-    
-    /**
-    * @generated
-    */
     public CanBeResearcher setResearcher(CanBeResearcher researcher) {
         this.researcher = researcher;
     }
-    
-    /**
-    * @generated
-    */
     private CanBeResearcher getResearcher() {
         return this.researcher;
     }
-    
-    /**
-    * @generated
-    */
     private CanBeResearcher setResearcher(CanBeResearcher researcher) {
         this.researcher = researcher;
     }
