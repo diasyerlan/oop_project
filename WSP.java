23ec5ee369d2782ed790c4a73b28ab9a8232e5e5
BatyrCode <122871602+Batyrq14@users.noreply.github.com>
2023-12-14 01:36:19 +0600

    Update WSP.java

--- WSP.java
+++ WSP.java
@@ -1,355 +1,169 @@
 package proj;
 
+import java.util.ArrayDeque;
+import java.util.Vector;
 
-/**
-* @generated
-*/
 public class WSP {
-    
-    /**
-    * @generated
-    */
     private Boolean isRegistration;
-    
-    /**
-    * @generated
-    */
     private WSP instance;
-    
-    /**
-    * @generated
-    */
     private Vector<User> listOfUSers;
-    
-    /**
-    * @generated
-    */
     private Vector<Student> listOfStudents;
-    
-    /**
-    * @generated
-    */
     private Vector<Manager> listOfManagers;
-    
-    /**
-    * @generated
-    */
     private Vector<Librarian> listOFLibrarians;
-    
-    /**
-    * @generated
-    */
     private Vector<Course> ActiveCourses;
-    
-    /**
-    * @generated
-    */
     private Vector<Course> listOfAllCourses;
-    
-    /**
-    * @generated
-    */
     private Vector<News> listOfNews;
-    
-    /**
-    * @generated
-    */
     private Vector<Book> listOfBooks;
-    
-    /**
-    * @generated
-    */
     private Vector<StudentOrganization> listOfStudentOrganizations;
-    
-    /**
-    * @generated
-    */
     private ArrayDeque<Request> listOfREquestsOR;
-    
-    /**
-    * @generated
-    */
     private ArrayDeque<Request> listOfRequestsFaculty;
-    
-    
-    /**
-    * @generated
-    */
     private User user;
-    
-    
-    /**
-    * @generated
-    */
+
     public Boolean getIsRegistration() {
         return this.isRegistration;
     }
-    
-    /**
-    * @generated
-    */
-    public Boolean setIsRegistration(Boolean isRegistration) {
+
+    public void setIsRegistration(Boolean isRegistration) {
         this.isRegistration = isRegistration;
     }
-    
-    /**
-    * @generated
-    */
+
     public WSP getInstance() {
         return this.instance;
     }
-    
-    /**
-    * @generated
-    */
-    public WSP setInstance(WSP instance) {
+
+    public void setInstance(WSP instance) {
         this.instance = instance;
     }
-    
-    /**
-    * @generated
-    */
+
     private Vector<User> getListOfUSers() {
         return this.listOfUSers;
     }
-    
-    /**
-    * @generated
-    */
-    private Vector<User> setListOfUSers(Vector<User> listOfUSers) {
+
+    private void setListOfUSers(Vector<User> listOfUSers) {
         this.listOfUSers = listOfUSers;
     }
-    
-    /**
-    * @generated
-    */
+
     private Vector<Student> getListOfStudents() {
         return this.listOfStudents;
     }
-    
-    /**
-    * @generated
-    */
-    private Vector<Student> setListOfStudents(Vector<Student> listOfStudents) {
+
+    private void setListOfStudents(Vector<Student> listOfStudents) {
         this.listOfStudents = listOfStudents;
     }
-    
-    /**
-    * @generated
-    */
+
     private Vector<Manager> getListOfManagers() {
         return this.listOfManagers;
     }
-    
-    /**
-    * @generated
-    */
-    private Vector<Manager> setListOfManagers(Vector<Manager> listOfManagers) {
+
+    private void setListOfManagers(Vector<Manager> listOfManagers) {
         this.listOfManagers = listOfManagers;
     }
-    
-    /**
-    * @generated
-    */
+
     private Vector<Librarian> getListOFLibrarians() {
         return this.listOFLibrarians;
     }
-    
-    /**
-    * @generated
-    */
-    private Vector<Librarian> setListOFLibrarians(Vector<Librarian> listOFLibrarians) {
+
+    private void setListOFLibrarians(Vector<Librarian> listOFLibrarians) {
         this.listOFLibrarians = listOFLibrarians;
     }
-    
-    /**
-    * @generated
-    */
+
     private Vector<Course> getActiveCourses() {
         return this.ActiveCourses;
     }
-    
-    /**
-    * @generated
-    */
-    private Vector<Course> setActiveCourses(Vector<Course> ActiveCourses) {
+
+    private void setActiveCourses(Vector<Course> ActiveCourses) {
         this.ActiveCourses = ActiveCourses;
     }
-    
-    /**
-    * @generated
-    */
+
     private Vector<Course> getListOfAllCourses() {
         return this.listOfAllCourses;
     }
-    
-    /**
-    * @generated
-    */
-    private Vector<Course> setListOfAllCourses(Vector<Course> listOfAllCourses) {
+
+    private void setListOfAllCourses(Vector<Course> listOfAllCourses) {
         this.listOfAllCourses = listOfAllCourses;
     }
-    
-    /**
-    * @generated
-    */
+
     private Vector<News> getListOfNews() {
         return this.listOfNews;
     }
-    
-    /**
-    * @generated
-    */
-    private Vector<News> setListOfNews(Vector<News> listOfNews) {
+
+    private void setListOfNews(Vector<News> listOfNews) {
         this.listOfNews = listOfNews;
     }
-    
-    /**
-    * @generated
-    */
+
     private Vector<Book> getListOfBooks() {
         return this.listOfBooks;
     }
-    
-    /**
-    * @generated
-    */
-    private Vector<Book> setListOfBooks(Vector<Book> listOfBooks) {
+
+    private void setListOfBooks(Vector<Book> listOfBooks) {
         this.listOfBooks = listOfBooks;
     }
-    
-    /**
-    * @generated
-    */
+
     private Vector<StudentOrganization> getListOfStudentOrganizations() {
         return this.listOfStudentOrganizations;
     }
-    
-    /**
-    * @generated
-    */
-    private Vector<StudentOrganization> setListOfStudentOrganizations(Vector<StudentOrganization> listOfStudentOrganizations) {
+
+    private void setListOfStudentOrganizations(Vector<StudentOrganization> listOfStudentOrganizations) {
         this.listOfStudentOrganizations = listOfStudentOrganizations;
     }
-    
-    /**
-    * @generated
-    */
+
     private ArrayDeque<Request> getListOfREquestsOR() {
         return this.listOfREquestsOR;
     }
-    
-    /**
-    * @generated
-    */
-    private ArrayDeque<Request> setListOfREquestsOR(ArrayDeque<Request> listOfREquestsOR) {
+
+    private void setListOfREquestsOR(ArrayDeque<Request> listOfREquestsOR) {
         this.listOfREquestsOR = listOfREquestsOR;
     }
-    
-    /**
-    * @generated
-    */
+
     private ArrayDeque<Request> getListOfRequestsFaculty() {
         return this.listOfRequestsFaculty;
     }
-    
-    /**
-    * @generated
-    */
-    private ArrayDeque<Request> setListOfRequestsFaculty(ArrayDeque<Request> listOfRequestsFaculty) {
+
+    private void setListOfRequestsFaculty(ArrayDeque<Request> listOfRequestsFaculty) {
         this.listOfRequestsFaculty = listOfRequestsFaculty;
     }
-    
 
-    //                          Operations                                  
-    
-    /**
-    * @generated
-    */
     public WSP getInstance() {
-        //TODO
         return null;
     }
-    
-    /**
-    * @generated
-    */
+
     public void readUniversityContainer() {
-        //TODO
-        return null;
+
     }
-    
-    /**
-    * @generated
-    */
+
     public void saveUniversityContainer() {
-        //TODO
-        return null;
+
     }
-    
-    /**
-    * @generated
-    */
+
     public String getNews() {
-        //TODO
         return "";
     }
-    
-    /**
-    * @generated
-    */
+
     public void saveNews() {
-        //TODO
-        return null;
+
     }
-    
-    /**
-    * @generated
-    */
+
     public String getBooks() {
-        //TODO
         return "";
     }
-    
-    /**
-    * @generated
-    */
+
     public void saveBooks() {
-        //TODO
-        return null;
+
     }
-    
-    /**
-    * @generated
-    */
+
     public Vector<Course> getActiveCourses() {
-        //TODO
         return null;
     }
-    
-    /**
-    * @generated
-    */
+
     public boolean isCourseAviable() {
-        //TODO
         return false;
     }
-    
-    /**
-    * @generated
-    */
+
     public boolean registerForCourse() {
-        //TODO
         return false;
     }
-    
-    /**
-    * @generated
-    */
+
     public void loadNews() {
-        //TODO
-        return null;
+
     }
-    
-    
 }
