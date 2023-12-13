fc582178e2d4b4606eeea605a902852e7412d910
YerasulGOD20 <sacterasyl@gmail.com>
2023-12-14 01:20:34 +0600

    Update Report.java

--- Report.java
+++ Report.java
@@ -1,86 +1,37 @@
 package proj;
 
 
-/**
-* @generated
-*/
 public class Report {
-    
-    /**
-    * @generated
-    */
+ 
     private String title;
-    
-    /**
-    * @generated
-    */
+   
     private User author;
     
-    /**
-    * @generated
-    */
     private String content;
     
-    
-    
-    /**
-    * @generated
-    */
     public String getTitle() {
         return this.title;
     }
     
-    /**
-    * @generated
-    */
     public String setTitle(String title) {
         this.title = title;
     }
-    
-    /**
-    * @generated
-    */
     public User getAuthor() {
         return this.author;
     }
-    
-    /**
-    * @generated
-    */
     public User setAuthor(User author) {
         this.author = author;
     }
-    
-    /**
-    * @generated
-    */
     private String getContent() {
         return this.content;
     }
-    
-    /**
-    * @generated
-    */
     private String setContent(String content) {
         this.content = content;
     }
-    
-
-    //                          Operations                                  
-    
-    /**
-    * @generated
-    */
     public void addContent() {
-        //TODO
         return null;
     }
-    
-    /**
-    * @generated
-    */
     public void viewReport() {
-        //TODO
         return null;
     }
     
