5293472b5d51a053db9110eed1244f3429e356c2
BatyrCode <122871602+Batyrq14@users.noreply.github.com>
2023-12-14 01:34:28 +0600

    Update User.java

--- User.java
+++ User.java
@@ -1,331 +1,157 @@
 package proj;
 
-
-/**
-* @generated
-*/
 public class User {
-    
-    /**
-    * @generated
-    */
     private String firstName;
-    
-    /**
-    * @generated
-    */
     private String lastName;
-    
-    /**
-    * @generated
-    */
     private String username;
-    
-    /**
-    * @generated
-    */
     private String email;
-    
-    /**
-    * @generated
-    */
     private String password;
-    
-    /**
-    * @generated
-    */
     private String ID;
-    
-    
-    /**
-    * @generated
-    */
+
     private News news;
-    
-    /**
-    * @generated
-    */
     private WSP wSP;
-    
-    
-    /**
-    * @generated
-    */
+
     private String getFirstName() {
         return this.firstName;
     }
-    
-    /**
-    * @generated
-    */
-    private String setFirstName(String firstName) {
+
+    private void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-    
-    /**
-    * @generated
-    */
+
     private String getLastName() {
         return this.lastName;
     }
-    
-    /**
-    * @generated
-    */
-    private String setLastName(String lastName) {
+
+    private void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    
-    /**
-    * @generated
-    */
+
     private String getUsername() {
         return this.username;
     }
-    
-    /**
-    * @generated
-    */
-    private String setUsername(String username) {
+
+    private void setUsername(String username) {
         this.username = username;
     }
-    
-    /**
-    * @generated
-    */
+
     private String getEmail() {
         return this.email;
     }
-    
-    /**
-    * @generated
-    */
-    private String setEmail(String email) {
+
+    private void setEmail(String email) {
         this.email = email;
     }
-    
-    /**
-    * @generated
-    */
+
     private String getPassword() {
         return this.password;
     }
-    
-    /**
-    * @generated
-    */
-    private String setPassword(String password) {
+
+    private void setPassword(String password) {
         this.password = password;
     }
-    
-    /**
-    * @generated
-    */
+
     private String getID() {
         return this.ID;
     }
-    
-    /**
-    * @generated
-    */
-    private String setID(String ID) {
+
+    private void setID(String ID) {
         this.ID = ID;
     }
-    
 
-    //                          Operations                                  
-    
-    /**
-    * @generated
-    */
     public boolean changePassword() {
-        //TODO
         return false;
     }
-    
-    /**
-    * @generated
-    */
+
     public void updateLogin() {
-        //TODO
-        return null;
+
     }
-    
-    /**
-    * @generated
-    */
+
     public boolean login() {
-        //TODO
         return false;
     }
-    
-    /**
-    * @generated
-    */
+
     public void viewNews() {
-        //TODO
-        return null;
+
     }
-    
-    /**
-    * @generated
-    */
+
     public void createRequest() {
-        //TODO
-        return null;
+
     }
-    
-    /**
-    * @generated
-    */
+
     public String toString() {
-        //TODO
         return "";
     }
-    
-    /**
-    * @generated
-    */
+
     public void showNews() {
-        //TODO
-        return null;
+
     }
-    
-    /**
-    * @generated
-    */
+
     public boolean verifyPassword() {
-        //TODO
         return false;
     }
-    
-    /**
-    * @generated
-    */
+
     public User findUser() {
-        //TODO
         return null;
     }
-    
-    /**
-    * @generated
-    */
+
     public boolean logOutSystem() {
-        //TODO
         return false;
     }
-    
-    /**
-    * @generated
-    */
+
     public void logInSystem() {
-        //TODO
-        return null;
+
     }
-    
-    /**
-    * @generated
-    */
+
     public void setEmail() {
-        //TODO
-        return null;
+
     }
-    
-    /**
-    * @generated
-    */
+
     public String getEmail() {
-        //TODO
         return "";
     }
-    
-    /**
-    * @generated
-    */
+
     public void setLastName() {
-        //TODO
-        return null;
+
     }
-    
-    /**
-    * @generated
-    */
+
     public String getLastName() {
-        //TODO
         return "";
     }
-    
-    /**
-    * @generated
-    */
+
     public void setFirstName() {
-        //TODO
-        return null;
+
     }
-    
-    /**
-    * @generated
-    */
+
     public String getFirstName() {
-        //TODO
         return "";
     }
-    
-    /**
-    * @generated
-    */
+
     public void setID() {
-        //TODO
-        return null;
+
     }
-    
-    /**
-    * @generated
-    */
+
     public String getID() {
-        //TODO
         return "";
     }
-    
-    /**
-    * @generated
-    */
+
     public void setPassword() {
-        //TODO
-        return null;
+
     }
-    
-    /**
-    * @generated
-    */
+
     public String getPassword() {
-        //TODO
         return "";
     }
-    
-    /**
-    * @generated
-    */
+
     public void setLogin() {
-        //TODO
-        return null;
+
     }
-    
-    /**
-    * @generated
-    */
+
     public String getUsername() {
-        //TODO
         return "";
     }
-    
-    /**
-    * @generated
-    */
+
     public String generate_report() {
-        //TODO
         return "";
     }
-    
-    
 }
