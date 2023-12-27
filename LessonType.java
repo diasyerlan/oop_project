e691469fbedae77ed042dd38f4ae88daa6ff8487
YerasulGOD20 <sacterasyl@gmail.com>
2023-12-27 14:14:21 +0600

    Update LessonType.java

--- LessonType.java
+++ LessonType.java
@@ -1,5 +1,8 @@
 package proj;
-
+/**
+ * The LessonType enum represents different types of lessons, such as LECTURE, PRACTICE, and LABORATORY_WORK.
+ * It is used to categorize and identify the type of a particular lesson.
+ */
 public enum LessonType {
 	LECTURE, PRACTICE, LABARATORYWORK
 }
