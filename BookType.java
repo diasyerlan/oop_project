c91973151265bf4e8c7d2ca36e13ac8f0599e955
YerasulGOD20 <sacterasyl@gmail.com>
2023-12-27 14:13:45 +0600

    Update BookType.java

--- BookType.java
+++ BookType.java
@@ -1,6 +1,9 @@
 package proj;
-
+/**
+ * Enumeration representing different types of books, including ELECTRONIC and PRINTED.
+ */
 public enum BookType {
-	ELECTRONIC, PRINTED
+	ELECTRONIC,// Represents an electronic book.
+	PRINTED// Represents a printed (physical) book.
 	
 }
