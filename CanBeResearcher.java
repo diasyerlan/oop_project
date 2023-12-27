84f8be945be05ce5cd2944ce2a3ce2dc8ed7a6a1
YerasulGOD20 <sacterasyl@gmail.com>
2023-12-27 14:13:49 +0600

    Update CanBeResearcher.java

--- CanBeResearcher.java
+++ CanBeResearcher.java
@@ -2,15 +2,62 @@ package proj;
 
 import java.io.Serializable;
 
+import java.io.Serializable;
+
+/**
+ * An interface representing entities that can be researchers.
+ * Classes implementing this interface should provide methods to retrieve basic information
+ * such as first name, last name, username, email, password, ID, and degree.
+ */
 public interface CanBeResearcher extends Serializable {
 
+    /**
+     * Gets the first name of the researcher.
+     *
+     * @return The first name of the researcher.
+     */
     String getFirstName();
-    String getLastName();
-    String getUsername();
-    String getEmail();
-    String getPassword();
-    String getID();
-    Degree getDegree();
 
-    //void display();
+    /**
+     * Gets the last name of the researcher.
+     *
+     * @return The last name of the researcher.
+     */
+    String getLastName();
+
+    /**
+     * Gets the username of the researcher.
+     *
+     * @return The username of the researcher.
+     */
+    String getUsername();
+
+    /**
+     * Gets the email of the researcher.
+     *
+     * @return The email of the researcher.
+     */
+    String getEmail();
+
+    /**
+     * Gets the password of the researcher.
+     *
+     * @return The password of the researcher.
+     */
+    String getPassword();
+
+    /**
+     * Gets the ID of the researcher.
+     *
+     * @return The ID of the researcher.
+     */
+    String getID();
+
+    /**
+     * Gets the degree of the researcher.
+     *
+     * @return The degree of the researcher.
+     */
+    Degree getDegree();
 }
+
