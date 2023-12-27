84e673685494c01fb7591475b868d11583f57025
YerasulGOD20 <sacterasyl@gmail.com>
2023-12-27 14:14:10 +0600

    Update EmployeeFactory.java

--- EmployeeFactory.java
+++ EmployeeFactory.java
@@ -3,7 +3,21 @@ package proj;
 public class EmployeeFactory {
 
     // Operations
-
+    /**
+     * Creates and returns an instance of the specified type of employee.
+     *
+     * @param employeeType    The type of employee to create.
+     * @param firstName       The first name of the employee.
+     * @param lastName        The last name of the employee.
+     * @param username        The username of the employee.
+     * @param email           The email of the employee.
+     * @param password        The password of the employee.
+     * @param ID              The ID of the employee.
+     * @param salary          The salary of the employee.
+     * @param hireDate        The hire date of the employee.
+     * @param workExperience  The work experience of the employee.
+     * @return An instance of the specified type of employee.
+     */
     public Employee getEmployee(String employeeType, String firstName, String lastName,
                                 String username, String email, String password, String ID,
                                 Integer salary, Integer hireDate, Integer workExperience) {
