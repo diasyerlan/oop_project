1ce1fe356efce51e9d0ab43bd59bd2bdaaea7e05
YerasulGOD20 <sacterasyl@gmail.com>
2023-12-27 14:14:13 +0600

    Update Faculty.java

--- Faculty.java
+++ Faculty.java
@@ -1,7 +1,10 @@
 package proj;
 
 import java.io.Serializable;
-
+/**
+ * The Faculty enum represents different faculties within an educational institution.
+ * Each faculty is identified by a specific code.
+ */
 public enum Faculty implements Serializable {
 	FIT, BS, KMA, SPE, ISE, SG
 }
