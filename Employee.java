e13f095bbc940b083f0b0ae2912b18aad09ef7cb
YerasulGOD20 <sacterasyl@gmail.com>
2023-12-27 14:14:08 +0600

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -8,7 +8,10 @@ import java.util.Vector;
 import java.nio.Buffer;
 import java.io.*;
 import java.util.*;
-
+/**
+ * The Employee class represents an employee in the system, extending the User class.
+ * It includes additional attributes such as hire date and work experience.
+ */
 public class Employee extends User {
     private static final long serialVersionUID = 5958066297683139454L;
     private Date hireDate;
@@ -19,6 +22,11 @@ public class Employee extends User {
     private String language;
     private static Employee instance;
     private static Map<Integer, String> words;
+    /**
+     * Sets the language for the employee instance.
+     *
+     * @param language The language code (e.g., "en", "kz", "ru").
+     */
     public void setLanguage(String language) {
         this.language = language;
 
@@ -33,7 +41,22 @@ public class Employee extends User {
     }
 
     // Constructors
+    /**
+     * Default constructor for the Employee class.
+     */
     public Employee(){}
+    /**
+     * Parameterized constructor for the Employee class.
+     *
+     * @param firstName      The first name of the employee.
+     * @param lastName       The last name of the employee.
+     * @param username       The username of the employee.
+     * @param email          The email of the employee.
+     * @param password       The password of the employee.
+     * @param ID             The ID of the employee.
+     * @param hireDate       The hire date of the employee.
+     * @param workExperience The work experience of the employee.
+     */
     public Employee(String firstName, String lastName, String username, String email,
                     String password, String ID, Date hireDate, Integer workExperience) {
         super(firstName, lastName, username, email, password, ID);
@@ -42,23 +65,43 @@ public class Employee extends User {
     }
 
     // Getters and setters
-
+    /**
+     * Gets the hire date of the employee.
+     *
+     * @return The hire date of the employee.
+     */
     public Date getHireDate() {
         return this.hireDate;
     }
 
+    /**
+     * Sets the hire date of the employee.
+     *
+     * @param hireDate The new hire date of the employee.
+     */
     public void setHireDate(Date hireDate) {
         this.hireDate = hireDate;
     }
-
+    /**
+     * Gets the work experience of the employee.
+     *
+     * @return The work experience of the employee.
+     */
     public Integer getWorkExperience() {
         return this.workExperience;
     }
 
+    /**
+     * Sets the work experience of the employee.
+     *
+     * @param workExperience The new work experience of the employee.
+     */
     public void setWorkExperience(Integer workExperience) {
         this.workExperience = workExperience;
     }
-
+    /**
+     * Static method to update employee information.
+     */
     // Operations
     public static void updateEmployee() {
         System.out.println(words.get(126));
